a7b9fe8287ca50076634be8cb3980ee42d7a26d0
Matheus Loran <111586873+Loran94@users.noreply.github.com>
2023-12-03 21:43:29 -0300

    server

--- models/index.js
+++ models/index.js
@@ -8,6 +8,8 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const express = require("express");
+
 
 let sequelize;
 if (config.use_env_variable) {
@@ -37,7 +39,9 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = db , app;
